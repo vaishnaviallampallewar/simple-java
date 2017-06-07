baa57c82bf8edbf50b18638e51ff7be11c3588dd
Vaishnavi Allampallewar <vaishnavi_a@persistent.co.in>
2017-06-07 15:23:25 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class HelloWorld{
-  public static void main(String[] args){
-    int[] arr = {Integer.parseInt(args[0]), Integer.parseInt(args[1])};
-    for(int i = 0; i< 2 ; i++){ 
-     System.out.println("Hello Vaishnavi for "+arr[i]+" time"); 
-  }
- }
-}
